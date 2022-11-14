e40f00e9d72a05837558ad04eb79926ed8dbfa97
Zach Miller <home@mac-2.local>
2022-11-13 20:21:55 -0500

    nuance additions, error catch on search

--- explorer/components/search.tsx
+++ explorer/components/search.tsx
@@ -1,25 +1,43 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 
 export const Search = () => {
   const router = useRouter()
+  const [NotFound, setNotFound] = useState(false)
+  const [Active, setActive] = useState(false)
 
   const searchAll = async (e: any) => {
     e.preventDefault()
     let send:any;
     const data = new FormData(e.target);
     const input = String(data.get("search"))
+
+    try {
     if (input.length === 66) {
       send = "/txn/" + input
     } else if (input.length === 42) {
       send = "/address/" + input
     } else if (/^\d+$/.test(input)) {
       send = "/block/" + input
-    } else {
-      alert("failed")
-    }
+    } 
+
     router.push(send);
+
+    } catch (err) {
+      console.log(err)
+      setNotFound(true)
+      setTimeout(() => {
+        setNotFound(false)
+      }, 4000);
     }
+    
+    }
+
+    const handleClick = (bool:boolean) => {
+      setActive(bool)
+    }
+  
 
 
 
@@ -28,22 +46,28 @@ export const Search = () => {
     <a href="/" className="font-bold mobile:text-[20px] laptop:text-[50px] flex flex-row w-fit"><img className="w-[7%] mr-3" src="/sparq_logo.svg" /> Subplorer</a>
     <form className="flex mt-3 flex-row mobile:w-full" onSubmit={searchAll}>
     <input
+      onFocus={() => handleClick(true)}
+      onBlur={() => handleClick(false)}
       id="search"
+      autoComplete="off"
       name="search"
       type="string"
       list="promotions"
       placeholder="Search by Address / Txn Hash / Block"
-      className=" border rounded-l-full px-3 py-2 mobile:w-full laptop:w-1/3 focus:outline-none focus:border-[#05e69f]"
+      className=" border rounded-l-md px-3 py-2 mobile:w-full laptop:w-1/3 focus:outline-none focus:border-[#05e69f]"
     />
-<datalist id="promotions">
-<span className="flex flex-row items-center bg-slate-300 px-3 py-2">Promotion</span> <a href="https://twitter.com/SparqNet/">Follow us on Twitter</a>
-</datalist>
-
 
     <button type="submit" className="rounded-r-full bg-[#05e69f] flex flex-row justify-center items-center">
       <img className=" mx-3 my-2 w-[22px]" src="/search.svg" />
     </button>
     </form>
+    { Active === true ?
+    <a href="https://twitter.com/SparqNet" className="relative laptop:w-1/3 flex flex-row items-center bg-slate-300 px-3 py-4">Promoted - Follow us on Twitter!</a>
+    : <></> }
+
+    { NotFound === true ? 
+      <h1 className="mt-3 text-[red]">Not Found</h1> : <></>
+  }
     </>
 
   )
